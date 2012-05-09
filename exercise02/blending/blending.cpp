4782c36976c945932f7a37fd550b03f4c163c47e
Andreas Pfohl <andreas@pf0hl.de>
2012-05-09 17:12:58 +0200

    Blending completed.

--- exercise02/blending/blending.cpp
+++ exercise02/blending/blending.cpp
@@ -3,7 +3,6 @@
 int width = 600;
 int height = 600;
 
-
 void drawQuad(float x, float y, float z)
 {
 	glBegin(GL_QUADS);
@@ -14,23 +13,47 @@ void drawQuad(float x, float y, float z)
 	glEnd();
 }
 
-
 void display(void)	
 {
+	/* Aufgabe a, b */
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	
+	/* Aufgabe c */
+	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glLoadIdentity();
 	gluLookAt(0, 0, 1, 0, 0, 0, 0, 1, 0);
 
 	// *** Farben mit Alpha Kanal setzen
+	
+	/* Aufgabe a */
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 	drawQuad(1, 1, -2);
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
 	drawQuad(0.25, 0.75, -1);
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
 	drawQuad(0.5, 0.25, 0);
+
+	/* Aufgabe b */
+	/*glColor4f(1.0f, 0.0f, 0.0f, 0.7f);
+	drawQuad(1, 1, -2);
+	glColor4f(0.0f, 1.0f, 0.0f, 0.7f);
+	drawQuad(0.25, 0.75, -1);
+	glColor4f(0.0f, 0.0f, 1.0f, 0.7f);
+	drawQuad(0.5, 0.25, 0);*/
+
+	/* Aufgabe c */
+	/*glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+	drawQuad(1, 1, -2);
+	glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
+	drawQuad(0.25, 0.75, -1);
+	glColor4f(1.0f, 0.0f, 1.0f, 1.0f);
+	drawQuad(0.5, 0.25, 0);*/
 
 	glFlush();
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -43,7 +66,7 @@ int main(int argc, char **argv)
 
 	glDisable(GL_DEPTH_TEST);
 
-	glViewport(0,0,width,height);
+	glViewport(0, 0, width, height);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -52,7 +75,18 @@ int main(int argc, char **argv)
 	glLoadIdentity();
 
 	// *** Blending Funktion setzen
+	glEnable(GL_BLEND);
+
+	/* Aufgabe a */
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+
+	/* Aufgabe b */
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);	
+
+	/* Aufgabe c */
+	//glBlendFunc(GL_DST_COLOR, GL_SRC_COLOR);
 
 	glutMainLoop();
+
 	return 0;
 }
