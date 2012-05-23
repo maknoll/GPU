04dc5112ac834e8a2f28a34f4557880bb1b9be5f
Martin Knoll <martinknoll@me.com>
2012-05-23 22:34:23 +0200

    vbo_fractal Aufgabe

--- exercise02/vbo_fractal/vbo_fractal.cpp
+++ exercise02/vbo_fractal/vbo_fractal.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 
+#include <GL/glew.h>
 #include <GLUT/glut.h>
 
 #include "vector.h"
@@ -67,21 +68,17 @@ Vector startP3(0.0f, 1.0f, 0.0f);
 int globalIndex = 0;
 int maxLevel = 8;
 
-GLuint positionsBuffer;
-GLuint normalsBuffer;
-GLuint indexBuffer;
-
 void generateTriangle(Vector p0, Vector p1, Vector p2)
 {
 	Triangle tri = Triangle(p0, p1, p2); 
 	positions.push_back(p0);
 	positions.push_back(p1);
 	positions.push_back(p2);
-
+  
 	normals.push_back(tri.getNormal());
 	normals.push_back(tri.getNormal());
 	normals.push_back(tri.getNormal());
-
+  
 	indices.push_back(globalIndex);
 	globalIndex++;
 	indices.push_back(globalIndex);
@@ -94,12 +91,12 @@ void generateTetraeders(Vector p0, Vector p1, Vector p2, Vector p3, int level)
 {	
 	if (level == maxLevel)
 		return;
-
+  
 	generateTriangle(p0, p1, p3);
 	generateTriangle(p1, p2, p3);
 	generateTriangle(p2, p0, p3);
 	generateTriangle(p0, p2, p1);
-
+  
 	Vector e0 = p1 - p0;
 	Vector e1 = p2 - p1;
 	Vector e2 = p2 - p0;
@@ -108,122 +105,84 @@ void generateTetraeders(Vector p0, Vector p1, Vector p2, Vector p3, int level)
 	Vector e5 = p3 - p1;
 	
 	Vector newP0, newP1, newP2, newP3;
-
+  
 	newP0 = p0;
 	newP1 = p0 - 0.5 * e0;
 	newP2 = p0 - 0.5 * e2;
 	newP3 = p0 - 0.5 * e3;
-
+  
 	generateTetraeders(newP0, newP2, newP1, newP3, level+1);
-
+  
 	newP0 = p1 + 0.5 * e0;
 	newP1 = p1;
 	newP2 = p1 - 0.5 * e1;
 	newP3 = p1 - 0.5 * e5;
-
+  
 	generateTetraeders(newP1, newP0, newP2, newP3, level+1);
-
+  
 	newP0 = p2 + 0.5 * e2;
 	newP1 = p2 + 0.5 * e1;
 	newP2 = p2;
 	newP3 = p2 - 0.5 * e4;
-
+  
 	generateTetraeders(newP2, newP1, newP0, newP3, level+1);
-
+  
 	newP0 = p3 + 0.5 * e3;
 	newP1 = p3 + 0.5 * e5;
 	newP2 = p3 + 0.5 * e4;
 	newP3 = p3;
-
+  
 	generateTetraeders(newP0, newP2, newP1, newP3, level+1);
 }
 
 void generateGeometryVertexBuffer()
 {
-
-	// TODO: Erzeugen eines Vertex Buffer Objects für die Positionen.
-	// Hinweis: Die Erzeugung eiens VBOs vollzieht sich in 4 Schritten
-	// - Generieren eines Buffer Handles
-	// - Binden des Buffers (Target = GL_ARRAY_BUFFER)
-	// - Buffer Daten in den VRAM kopieren (Usage = GL_STATIC_DRAW)
-	// - Buffer nicht mehr binden (stattdessen eine 0 binden).	
-
-  glGenBuffers(1, &positionsBuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, positionsBuffer);
+  glGenBuffers(1, &positionBufferHandle);
+  glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
   glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(Vector), &positions[0], GL_STATIC_DRAW);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 	
-	// TODO: Erzeugen eines Vertex Buffer Objects für die Normalen.
-  glGenBuffers(1, &normalsBuffer);
-  glBindBuffer(GL_NORMAL_ARRAY, normalsBuffer);
-  glBufferData(GL_NORMAL_ARRAY, normals.size() * sizeof(Vector), &normals[0], GL_STATIC_DRAW);
-  glBindBuffer(GL_NORMAL_ARRAY, 0);
-
-	// TODO: Erzeugen eines Indexbuffers. Dies funktioniert genauso, wie die Erzeugung eines VBOs (Hinweis: Target = GL_ELEMENT_ARRAY_BUFFER).
-  glGenBuffers(1, &indexBuffer);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+  glGenBuffers(1, &normalBufferHandle);
+  glBindBuffer(GL_ARRAY_BUFFER, normalBufferHandle);
+  glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(Vector ), &normals[0], GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  
+  glGenBuffers(1, &indexBufferHandle);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferHandle);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(int), &indices[0], GL_STATIC_DRAW);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	
-	// TODO: Erzeugen eines Vertex Array Objects (VAOs).
-	// VAOs werden genutzt um der GPU mitzuteilen, von welchen VBOs die Daten für das Rendering genommen werden sollen.
-	// - Generieren eines Vertex Array Handles
-	// - Binden des Vertex-Array Handles
-		// Binden des VBOs, von dem die Positionen geholt werden sollen
-		// mit glVertexAttribPointer der GPU mitteilen, wie das VBO interpretiert werden soll. Im folgenden werden die Parameter kurz erklärt.
-		//   Der erste Parameter ist der Index des Vertex-Attributes. (0=Position, 2=Normale)
-		//   Daraufhin folgen die Dimension der Daten (z.B. bei Positions = 3 (wegen xyz)) und der Datentyp.
-		//   Die Eingabedaten müssen nicht normalisiert werden.
-		//   Der Stride misst den Offset in Byte zwischen zwei Datenwerten im Buffer.
-		//   Der letzte Wert ist der BufferOffset, der angewendet werden soll. In unserem Beispiel ist er immer 0, da wir für jedes Attribut einen eigenes VBO nutzen.
-		// Binden des VBOs, von dem die Positionen geholt werden sollen
-		// wieder glVertexAttribPointer nutzen..
-		// Aufräumen: Den Wert '0' als Vertex Buffer Object binden. (kein VBO mehr aktiv)
-		// Zuletzt muss mitgeteilt werden, welche Vertex-Attribut Indices genutzt werden sollen. Dies geschieht mit glEnableVertexAttribArray.
-	// Aufräumen: Unbinden des Vertex Array Objects (eine 0 binden).
-  glBindBuffer(GL_ARRAY_BUFFER, positionsBuffer);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  
+  glGenVertexArraysAPPLE(1, &vertexArrayHandle);
+  glBindVertexArrayAPPLE(vertexArrayHandle);
+  glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vector), 0);
   glEnableVertexAttribArray(0);
-  glBindBuffer(GL_NORMAL_ARRAY, normalsBuffer);
-  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glBindBuffer(GL_ARRAY_BUFFER, normalBufferHandle);
+  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vector), 0);
   glEnableVertexAttribArray(2);
-
+  glBindVertexArrayAPPLE(0);
 }
 
 
 void drawGeometryVertexBuffer()
 {
-	// TODO: Binden des VAOs. (Die GPU weiß nun, wo sie die Geometriedaten herzuholen hat.)
-  // TODO: Index-Buffer binden.
-  glBindBuffer(GL_ARRAY_BUFFER, positionsBuffer);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-  glBindBuffer(GL_NORMAL_ARRAY, normalsBuffer);
-	
-	// TODO: mit glDrawElements die Triangles rendern. 
-	// Hinweis: Der Type-Parameter definiert den im Index Buffer verwendeten Datentyp.
-	//   Der letzte Parameter dieses DrawCalls ist NULL, da der Index-Buffer von Beginn an gelesen werden soll.
+  glBindVertexArrayAPPLE(vertexArrayHandle);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferHandle);
 	unsigned int numTriangles = positions.size();
 	
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_NORMAL_ARRAY);
-    glDrawElements(GL_TRIANGLES, numTriangles, GL_UNSIGNED_INT, (void *)0);
-    glDisableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_NORMAL_ARRAY);
-
-	// TODO: Unbinden von VAO und Index-Buffer	
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glBindVertexArrayAPPLE(0);
-
+  glDrawElements(GL_TRIANGLES, numTriangles, GL_UNSIGNED_INT, (void *)0);
+  
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  glBindVertexArrayAPPLE(0); 
 }
 
 // calc the view position and direction from theta/phi coordinates
 void calcViewerCamera(float theta, float phi, float r)
 {
-    float x = r * sin(theta) * cos(phi);
-    float y = r * cos(theta);
-    float z = r * sin(theta) * sin(phi);
- 
+  float x = r * sin(theta) * cos(phi);
+  float y = r * cos(theta);
+  float z = r * sin(theta) * sin(phi);
+  
 	viewPosition[0] = center[0] + x;
 	viewPosition[1] = center[1] + y;
 	viewPosition[2] = center[2] + z;
@@ -235,30 +194,30 @@ void calcViewerCamera(float theta, float phi, float r)
 void display()
 {
 	int timeStart = glutGet(GLUT_ELAPSED_TIME);
-
+  
 	// clear buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+  
 	calcViewerCamera(theta, phi, r);
-
+  
 	// now render from camera view
 	glLoadIdentity();
  	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
-  				
+  
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(viewPosition[0], viewPosition[1], viewPosition[2],
-			  viewPosition[0] + viewDirection[0], viewPosition[1] + viewDirection[1], viewPosition[2] + viewDirection[2], 
-			  0, 1, 0);
-
-
-
+            viewPosition[0] + viewDirection[0], viewPosition[1] + viewDirection[1], viewPosition[2] + viewDirection[2], 
+            0, 1, 0);
+  
+  
+  
 	if (useVBO) {    // fast drawing
-
+    
 		drawGeometryVertexBuffer();
 	}
 	else {  // simple and slow drawing
-
+    
 		glBegin(GL_TRIANGLES);
 		for (unsigned int i = 0 ; i < positions.size() ; i += 3) {
 			glNormal3fv(&normals[i][0]);
@@ -269,15 +228,15 @@ void display()
 			glVertex3fv(&positions[i+2][0]);
 		}
 		glEnd();
-
+    
 	}
-
+  
 	// glutSolidTeapot(1.0);
-
+  
 	// swap display buffers
 	glutSwapBuffers();
 	glFinish();
-
+  
 	// measure frame time in milliseconds
 	int timeEnd = glutGet(GLUT_ELAPSED_TIME);
 	printf("Delay %d     \r",timeEnd - timeStart);
@@ -291,10 +250,10 @@ void mouseMotion(int x, int y)
 	
 	if (motionState == ROTATE) {
 		theta -= 0.002f * deltaY;
-
+    
 		if (theta < 0.002f) theta = 0.002f;
 		else if (theta > PI - 0.002f) theta = PI - 0.002f;
-
+    
 		phi += 0.002f * deltaX;	
 		if (phi < 0) phi += 2*PI;
 		else if (phi > 2*PI) phi -= 2*PI;
@@ -303,10 +262,10 @@ void mouseMotion(int x, int y)
 		r += 0.05f * deltaY;
 		if (r < 0.01f) r = 0.01f;
 	}
-
+  
 	oldX = (float)x;
 	oldY = (float)y;
-
+  
 	glutPostRedisplay();
 }
 
@@ -314,7 +273,7 @@ void mouse(int button, int state, int x, int y)
 {
 	oldX = (float)x;
 	oldY = (float)y;
-
+  
 	if (button == GLUT_LEFT_BUTTON) {
 		if (state == GLUT_DOWN) {
 			motionState = ROTATE;
@@ -330,11 +289,11 @@ void mouse(int button, int state, int x, int y)
 void keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
-	case ' ':
-		useVBO = !useVBO;
-		break;
+    case ' ':
+      useVBO = !useVBO;
+      break;
 	}
-
+  
 }
 void initGL()
 {
@@ -343,34 +302,34 @@ void initGL()
 	glDepthFunc(GL_LESS);
 	glEnable(GL_LIGHTING);               
 	glEnable(GL_LIGHT0);                 
-
+  
 	glViewport(0,0,width,height);
-
+  
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-    gluPerspective(viewAngle, 1.0f, viewNear, viewFar);
-
+  gluPerspective(viewAngle, 1.0f, viewNear, viewFar);
+  
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
+  
 	glClearColor(0.5, 0.5, 1.0, 1.0);
 #if 1
-    GLuint shaderProgram;
-    GLchar *vertexSource = "void main(void){gl_FrontColor = gl_Color;gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;}";
-    GLchar *fragmentSource = "void main(void){gl_FragColor = gl_FragColor = vec4(ex_Color,1.0);}";
-    GLuint vertexShader, fragmentShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
-    glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
-    glCompileShader(vertexShader);
-    glCompileShader(fragmentShader);
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glBindAttribLocation(shaderProgram, 0, "in_Position");
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
+  GLuint shaderProgram;
+  GLchar *vertexSource = "void main(void){gl_FrontColor = gl_Color;gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;}";
+  GLchar *fragmentSource = "void main(void){gl_FragColor = gl_FragColor = vec4(ex_Color,1.0);}";
+  GLuint vertexShader, fragmentShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+  glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+  glCompileShader(vertexShader);
+  glCompileShader(fragmentShader);
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glBindAttribLocation(shaderProgram, 0, "in_Position");
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
 #endif
 }
 
@@ -381,11 +340,13 @@ int main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowSize(width, height);
-
+  
 	glutCreateWindow("Fractal with VBO");
-
+  
 	// Init glew so that the GLSL functionality will be available
-
+  if(glewInit() != GLEW_OK)
+    cout << "GLEW init failed!" << endl;
+  
 	// Register GLUT callback functions   
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
@@ -393,21 +354,21 @@ int main(int argc, char** argv)
 	glutKeyboardFunc(keyboard);
 	glutMotionFunc(mouseMotion);
 	glutMouseFunc(mouse);
-
-
+  
+  
 	// subdivideTriangle(startTriangle, 4.0, 0);
 	generateTetraeders(5.0*startP0, 5.0*startP1, 5.0*startP2, 5.0*startP3, 0);
-
+  
 	// we draw the geometry as a vertex buffer
 	generateGeometryVertexBuffer();
-
+  
 	initGL();
 	// Enter main loop
 	glutMainLoop();
-
+  
 	return 0;
-
-
-
+  
+  
+  
 }
 
