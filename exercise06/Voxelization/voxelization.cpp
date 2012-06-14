7fbac624374c17ffde3b8136d7ecaaa964d64764
Martin Knoll <martinknoll@me.com>
2012-06-14 04:25:50 +0200

    Hull Voxelization

--- exercise06/Voxelization/voxelization.cpp
+++ exercise06/Voxelization/voxelization.cpp
@@ -64,10 +64,10 @@ GLuint pixels[PIC_HEIGHT*PIC_WIDTH*4];
 // Print information about the compiling step
 void printShaderInfoLog(GLuint shader)
 {
-    GLint infologLength = 0;
-    GLsizei charsWritten  = 0;
-    char *infoLog;
-
+  GLint infologLength = 0;
+  GLsizei charsWritten  = 0;
+  char *infoLog;
+  
 	glGetShaderiv(shader, GL_INFO_LOG_LENGTH,&infologLength);		
 	infoLog = (char *)malloc(infologLength);
 	glGetShaderInfoLog(shader, infologLength, &charsWritten, infoLog);
@@ -81,7 +81,7 @@ void printProgramInfoLog(GLuint program)
 	GLint infoLogLength = 0;
 	GLsizei charsWritten  = 0;
 	char *infoLog;
-
+  
 	glGetProgramiv(program, GL_INFO_LOG_LENGTH,&infoLogLength);
 	infoLog = (char *)malloc(infoLogLength);
 	glGetProgramInfoLog(program, infoLogLength, &charsWritten, infoLog);
@@ -94,7 +94,7 @@ string readFile(string fileName)
 {
 	string fileContent;
 	string line;
-
+  
 	ifstream file(fileName.c_str());
 	if (file.is_open()) {
 		while (!file.eof()){
@@ -106,7 +106,7 @@ string readFile(string fileName)
 	}
 	else
 		cout << "ERROR: Unable to open file " << fileName << endl;
-
+  
 	return fileContent;
 }
 
@@ -116,39 +116,39 @@ void loadShaderProgram(GLuint &shaderProgram, GLuint &vertexShader, char* vertex
 {
 	// Create empty shader object (vertex shader)
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
+  
 	// Read vertex shader source 
 	string shaderSource = readFile(vertexShaderName);
 	const char* sourcePtr = shaderSource.c_str();
-
+  
 	// Attach shader code
 	glShaderSource(vertexShader, 1, &sourcePtr, NULL);	
-
+  
 	// Compile
 	glCompileShader(vertexShader);
 	printShaderInfoLog(vertexShader);
-
+  
 	// Create empty shader object (fragment shader)
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
+  
 	// Read vertex shader source 
 	shaderSource = readFile(fragmentShaderName);
 	sourcePtr = shaderSource.c_str();
-
+  
 	// Attach shader code
 	glShaderSource(fragmentShader, 1, &sourcePtr, NULL);	
-
+  
 	// Compile
 	glCompileShader(fragmentShader);
 	printShaderInfoLog(fragmentShader);
-
+  
 	// Create shader program
 	shaderProgram = glCreateProgram();	
-
+  
 	// Attach shader
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
-
+  
 	// Link program
 	glLinkProgram(shaderProgram);
 	printProgramInfoLog(shaderProgram);
@@ -166,29 +166,29 @@ int initFBOTextures()
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-
+  
 	// FBO anlegen und Texturen zuweisen
 	glGenFramebuffers (1, &voxelizationFB);
 	glBindFramebuffer (GL_FRAMEBUFFER, voxelizationFB);
 	glFramebufferTexture2D (GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, voxelizationTextureId, 0);
-
+  
 	// FBO Textur als aktive Textur binden.
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture (GL_TEXTURE_2D, voxelizationTextureId);
-
+  
 	// check framebuffer status
 	GLenum status = glCheckFramebufferStatus (GL_FRAMEBUFFER);
 	switch (status)
 	{
-	case GL_FRAMEBUFFER_COMPLETE:
-		cout << "FBO complete" << endl;
-		break;
-	case GL_FRAMEBUFFER_UNSUPPORTED:
-		cout << "FBO configuration unsupported" << endl;
-		return 1;
-	default:
-		cout << "FBO programmer error" << endl;
-		return 1;
+    case GL_FRAMEBUFFER_COMPLETE:
+      cout << "FBO complete" << endl;
+      break;
+    case GL_FRAMEBUFFER_UNSUPPORTED:
+      cout << "FBO configuration unsupported" << endl;
+      return 1;
+    default:
+      cout << "FBO programmer error" << endl;
+      return 1;
 	}
 	glBindFramebuffer (GL_FRAMEBUFFER, 0);
 	return 0;
@@ -198,10 +198,10 @@ int initFBOTextures()
 // calc the view position and direction from theta/phi coordinates
 void calcViewerCamera(float theta, float phi, float r)
 {
-    float x = r * sin(theta) * cos(phi);
-    float y = r * cos(theta);
-    float z = r * sin(theta) * sin(phi);
- 
+  float x = r * sin(theta) * cos(phi);
+  float y = r * cos(theta);
+  float z = r * sin(theta) * sin(phi);
+  
 	viewPosition[0] = center[0] + x;
 	viewPosition[1] = center[1] + y;
 	viewPosition[2] = center[2] + z;
@@ -223,57 +223,166 @@ void drawVoxel(float x, float y, float z)
 	float sizeX = 1.0f/(float)(VOXEL_WIDTH);
 	float sizeY = 1.0f/(float)(VOXEL_HEIGHT);
 	float sizeZ = 1.0f/128.0f;
-
+  
 	// TODO: Rendern Sie einen Cube mit dem Zentrum an der Position (x,y,z) und der Ausdehnung sizeX x sizeY x sizeZ.
-	// Um die Visualisierung deutlicher zu machen, sollen die Eckpunkte einer jeden Seitenwand unterschiedliche Farben haben.
+  glPushMatrix();
+  glTranslatef(x, y, z);
+  
+  float a = sizeX / 2;
+  float b = sizeY / 2;
+  float c = sizeZ / 2;
+  
+  GLfloat triangles[] = {
+    a, -b, -c,
+    a, b, -c,
+    a, -b, c,
+    a, -b, c,
+    a, b, -c,
+    a, b, c,
+    
+    a, b, -c,
+    -a, b, -b,
+    a, b, c,
+    a, b, c,
+    -a, b, -c,
+    -a, b, c,
+    
+    -a, b, -c,
+    -a, -b, -c,
+    -a, b, c,
+    -a, b, c,
+    -a, -b, -c,
+    -a, -b, c,
+    
+    -a, -b, -c,
+    a, -b, -c,
+    -a, -b, c,
+    -a, -b, b,
+    a, -b, -c,
+    a, -b, c,
+    
+    a, b, c,
+    -a, b, c,
+    a, -b, c,
+    a, -b, c,
+    -a, b, c,
+    -a, -b, c,
+    
+    a, -b, -c,
+    -a, -b, -c,
+    a, b, -c,
+    a, b, -c,
+    -a, -b, -c,
+    -a, b, -c
+  };
+  
+  GLubyte colors[] = {
+    0, 0, 255, 255,
+    0, 0, 255, 255,
+    0, 0, 255, 255,
+    0, 0, 255, 255,
+    0, 0, 255, 255,
+    0, 0, 255, 255,
+    
+    0, 255, 0, 255,
+    0, 255, 0, 255,
+    0, 255, 0, 255,
+    0, 255, 0, 255,
+    0, 255, 0, 255,
+    0, 255, 0, 255,
+    
+    255, 0, 0, 255,
+    255, 0, 0, 255,
+    255, 0, 0, 255,
+    255, 0, 0, 255,
+    255, 0, 0, 255,
+    255, 0, 0, 255,
+    
+    0, 255, 255, 255,
+    0, 255, 255, 255,
+    0, 255, 255, 255,
+    0, 255, 255, 255,
+    0, 255, 255, 255,
+    0, 255, 255, 255,
+    
+    255, 0, 255, 255,
+    255, 0, 255, 255,
+    255, 0, 255, 255,
+    255, 0, 255, 255,
+    255, 0, 255, 255,
+    255, 0, 255, 255,
+    
+    255, 255, 0, 255,
+    255, 255, 0, 255,
+    255, 255, 0, 255,
+    255, 255, 0, 255,
+    255, 255, 0, 255,
+    255, 255, 0, 255
+  };
+  
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, triangles);
+  glEnableVertexAttribArray(2);
+  glVertexAttribPointer(2, 4, GL_UNSIGNED_BYTE, GL_FALSE, 0, colors);
+  glDrawArrays (GL_TRIANGLES, 0, 36);
+  
+  glPopMatrix();
 }
 
 void drawVoxelModel()
 {
-	glBegin(GL_QUADS);
 
 	int i = 0;
 	for (int y = 0 ; y < VOXEL_HEIGHT ; y++) {
-
+    
 		for (int x = 0 ; x < VOXEL_WIDTH ; x++) {
-
+      
 			for (int k = 3 ; k >= 0 ; k--) {	// a,b,g,r
-
+        
 				unsigned int bitCode = pixels[i];	// 32-Bit Komponente lesen
 				i++;
-
+        
 				for (int z = 0 ; z < 32 ; z++) {	// jedes Bit einzeln durchgehen
 					
 					if (bitCode & 1) {	// wenn Bit gesetzt, zeichne ein Voxel
-
+            
 						drawVoxel(x/(float)(VOXEL_WIDTH) - 0.5f, y/(float)(VOXEL_HEIGHT) - 0.5f, 0.25f*(k-2 + z/32.0f));
 					}
-
+          
 					bitCode = bitCode >> 1;	// das nächste Bit betrachten
 				}
 			}
 		}
 	}
-	glEnd();
+
 }
 
 
 // OpenGL display Funktion
 void display()
 {
+  
 	int timeStart = glutGet(GLUT_ELAPSED_TIME);
-
+  
 	// ********** voxelize teapot into integer texture ************
 	
 	// TODO: FBO binden, in das gerendert werden soll. Clearen Sie das FBO und binden Sie den Voxelisierungs-Shader.
+  glBindFramebuffer (GL_FRAMEBUFFER, voxelizationFB);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glUseProgram(shaderProgramVoxelization);
 	
 	// TODO: Viewport auf Auflösung der Voxel-Textur setzen.	
-
+  
+  glViewport(0, 0, VOXEL_WIDTH, VOXEL_HEIGHT);
+  
 	// TODO: Tiefentest deaktivieren.
+  glDisable(GL_DEPTH_TEST);
 	
 	// TODO: Logik-Operation aktivieren. Anstatt den Farbwert in das Target zu schreiben, 
 	// werden die Komponenten des Pixels als UINTs aufgefasst und mit dem Pixel im FBO mit OR verknüpft ("reingeodert"...)	
-
+  glEnable(GL_COLOR_LOGIC_OP);
+  glLogicOp(GL_OR);
+  
 	// Projektionsmatrix setzen
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -282,23 +391,34 @@ void display()
 	// Viewmatrix auf Einheitsmatrix setzen
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();	
-
+  
 	// Kanne rotieren
 	glRotatef(angle, 0.0f, 1.0f, 0.0f);
 	angle += 0.1f;
-
+  
 	// Kanne zeichnen
 	glutSolidTeapot(0.7);
 	
 	// TODO: Rendern in FBO beenden (Backbuffer wieder aktiv) und Fixed-Function Pipeline aktivieren.
-
+  
 	// *************** read voxel texture for visualization *************
 	// TODO: Texturdaten der Voxelisierungs-Textur auslesen.
+  
+  glReadPixels(0, 0, VOXEL_WIDTH, VOXEL_HEIGHT, GL_RGBA_INTEGER, GL_UNSIGNED_INT, pixels);
 
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  glUseProgram(0);
+  
 	// **************** draw the voxel model ****************
 	// TODO: Viewport auf Bildschirmauflösung setzen und Backbuffer clearen.
+  
+  glViewport(0, 0, PIC_WIDTH, PIC_HEIGHT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	// TODO: Tiefentest an, Beleuchtung und Logik-Operationen aus.
+  glEnable(GL_DEPTH_TEST);
+  glDisable(GL_LIGHTING);
+  glDisable(GL_COLOR_LOGIC_OP);
 	
 	// Perspektivische Projektionsmatrix
 	glMatrixMode(GL_PROJECTION);
@@ -310,15 +430,15 @@ void display()
 	glLoadIdentity();
 	calcViewerCamera(theta, phi, r);
 	gluLookAt(viewPosition[0], viewPosition[1], viewPosition[2],
-			  viewPosition[0] + viewDirection[0], viewPosition[1] + viewDirection[1], viewPosition[2] + viewDirection[2], 
-			  0, 1, 0);
-
+            viewPosition[0] + viewDirection[0], viewPosition[1] + viewDirection[1], viewPosition[2] + viewDirection[2], 
+            0, 1, 0);
+  
 	// Voxel Model zeichnen
 	drawVoxelModel();
-
+  
 	// Szene ist fertig, Buffer swappen.
 	glutSwapBuffers();
-
+  
 	// Verstrichene Zeit ausgeben.
 	int timeEnd = glutGet(GLUT_ELAPSED_TIME);
 	printf("Delay %4d\r",timeEnd - timeStart);
@@ -331,26 +451,26 @@ void mouseMotion(int x, int y)
 	float deltaX = x - oldX;
 	float deltaY = y - oldY;
 	
-
-		if (motionState == ROTATE) {
-			theta -= 0.002f * deltaY;
-
-			if (theta < 0.002f) theta = 0.002f;
-			else if (theta > PI - 0.002f) theta = PI - 0.002f;
-
-			phi += 0.002f * deltaX;	
-			if (phi < 0) phi += 2*PI;
-			else if (phi > 2*PI) phi -= 2*PI;
-		}
-		else if (motionState == MOVE) {
-			r += 0.01f * deltaY;
-			if (r < 0.01f) r = 0.01f;
-		}
-
-
+  
+  if (motionState == ROTATE) {
+    theta -= 0.002f * deltaY;
+    
+    if (theta < 0.002f) theta = 0.002f;
+    else if (theta > PI - 0.002f) theta = PI - 0.002f;
+    
+    phi += 0.002f * deltaX;	
+    if (phi < 0) phi += 2*PI;
+    else if (phi > 2*PI) phi -= 2*PI;
+  }
+  else if (motionState == MOVE) {
+    r += 0.01f * deltaY;
+    if (r < 0.01f) r = 0.01f;
+  }
+  
+  
 	oldX = (float)x;
 	oldY = (float)y;
-
+  
 	glutPostRedisplay();
 }
 
@@ -358,7 +478,7 @@ void mouse(int button, int state, int x, int y)
 {
 	oldX = (float)x;
 	oldY = (float)y;
-
+  
 	if (button == GLUT_LEFT_BUTTON) {
 		if (state == GLUT_DOWN) {
 			motionState = ROTATE;
@@ -379,22 +499,22 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE | GLUT_MULTISAMPLE);
 	glutInitWindowSize(PIC_WIDTH, PIC_HEIGHT);
 	glutCreateWindow("GPU Voxelization");
-
+  
 	// Init glew so that the GLSL functionality will be available
 	if(glewInit() != GLEW_OK)
 		cout << "GLEW init failed!" << endl;
-
+  
 	initFBOTextures();
 	initGLSL();
-
+  
 	glutMotionFunc(mouseMotion);
 	glutMouseFunc(mouse);
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
-
-    glutMainLoop();
-                
-    return 0;
+  
+  glutMainLoop();
+  
+  return 0;
 }
 
 
--- exercise06/Voxelization/voxelization.frag
+++ exercise06/Voxelization/voxelization.frag
@@ -1,5 +1,5 @@
 // Extension aktivieren, damit << im Befehlssatz vorliegt.
-#extension GL_EXT_gpu_shader4 : enable
+#extension GL_EXT_gpu_shader4 : require
 
 // Ausgabevariable
 varying out uvec4 result;
@@ -7,6 +7,7 @@ varying out uvec4 result;
 void main()
 {	
 	// TODO: Tiefenwert von [0..1] auf {0..127} abbilden.
+  unsigned int depth = unsigned int(gl_FragCoord.z * 127.);
 	
 	// Dies ergibt beispielsweise den Wert 42.
 	// Erzeugen Sie nun eine bit-Maske, in der das (im Beispiel) 42te Bit (von rechts gezählt) eine 1 ist und alle anderen eine 0.
@@ -15,4 +16,14 @@ void main()
 	//                  ^
 	//                Bit 42
 	// Weisen Sie diese bit-Maske der Variable 'result' zu.
+  
+  if (depth < 32u)
+    result.w = 1u << depth;
+  else if (depth < 64u)
+    result.z = 1u << (depth % 32u);
+  else if (depth < 96u)
+    result.y = 1u << (depth % 32u);
+  else if (depth < 128u)
+    result.x = 1u << (depth % 32u);
+  
 }
